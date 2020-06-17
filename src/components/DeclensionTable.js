43c72906e25b30c9461303738ae3d7dbddba00a0
blinkous <saradhani1@gmail.com>
2020-06-17 17:12:26 -0400

    added all of the declension tables and their data

--- src/components/DeclensionTable.js
+++ src/components/DeclensionTable.js
@@ -3,27 +3,39 @@ import "../styles/DeclensionTable.css";
 import declensions from "../js/declensions";
 
 const DeclensionTable = (props) => {
-  const declension = declensions.first_declension;
+  const firstDecl = declensions.declensions.first_declension;
+  const allDeclensions = declensions.declensions;
 
   return (
-    <table className={props.classes || ""} id={props.id || ""}>
-      <thead>
-        <tr className="header">
-          <th className="case">Cases</th>
-          <th className="singular">Singular</th>
-          <th className="plural">Plural</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.entries(declension).map(([key, value], index) => (
-          <tr className={key} key={index}>
-            <td className={`case ${key}`}>{key}</td>
-            <td className={`singular ${value.singular}`}>{value.singular}</td>
-            <td className={`plural ${value.plural}`}>{value.plural}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      {Object.entries(allDeclensions).map(([key, value], index) => (
+        <table key={index} className={`declension-table ${key}`}>
+          <caption>
+            {key
+              .replace(/_/g, " ")
+              .replace(/^[a-z]|\s\w/g, (match) => match.toUpperCase())}
+          </caption>
+          <thead>
+            <tr className="header">
+              <th className="case">Cases</th>
+              <th className="singular">Singular</th>
+              <th className="plural">Plural</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(allDeclensions[key]).map(([key, value], index) => (
+              <tr className={key} key={index}>
+                <td className={`case ${key}`}>{key}</td>
+                <td className={`singular ${value.singular}`}>
+                  {value.singular}
+                </td>
+                <td className={`plural ${value.plural}`}>{value.plural}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ))}
+    </>
   );
 };
 export default DeclensionTable;
--- src/js/declensions.js
+++ src/js/declensions.js
@@ -1,56 +1,215 @@
+/* āēīōū */
 export default {
   definition:
     "The variation of a noun, pronoun, adjective or article to indicate grammatical case, number and gender.",
-  first_declension: {
-    nominative: {
-      singular: "a",
-      plural: "ae",
+  declensions: {
+    first_declension: {
+      nominative: {
+        singular: "a",
+        plural: "ae",
+      },
+      genitive: {
+        singular: "ae",
+        plural: "ārum",
+      },
+      dative: {
+        singular: "ae",
+        plural: "īs",
+      },
+      accusative: {
+        singular: "am",
+        plural: "ās",
+      },
+      ablative: {
+        singular: "ā",
+        plural: "īs",
+      },
+      vocative: {
+        singular: "a",
+        plural: "ae",
+      },
     },
-    genitive: {
-      singular: "ae",
-      plural: "ārum",
+    second_declension: {
+      nominative: {
+        singular: "us",
+        plural: "ī",
+      },
+      genitive: {
+        singular: "ī",
+        plural: "ōrum",
+      },
+      dative: {
+        singular: "ō",
+        plural: "īs",
+      },
+      accusative: {
+        singular: "um",
+        plural: "ōs",
+      },
+      ablative: {
+        singular: "ō",
+        plural: "īs",
+      },
+      vocative: {
+        singular: "us -> e | Name end ius -> ī",
+        plural: "ī",
+      },
     },
-    dative: {
-      singular: "ae",
-      plural: "īs",
+    neuter_second_declension: {
+      nominative: {
+        singular: "um",
+        plural: "a",
+      },
+      genitive: {
+        singular: "ī",
+        plural: "ōrum",
+      },
+      dative: {
+        singular: "ō",
+        plural: "īs",
+      },
+      accusative: {
+        singular: "um",
+        plural: "a",
+      },
+      ablative: {
+        singular: "ō",
+        plural: "īs",
+      },
+      vocative: {
+        singular: "us -> e | Name end ius -> ī",
+        plural: "a",
+      },
     },
-    accusative: {
-      singular: "am",
-      plural: "ās",
+    third_declension: {
+      nominative: {
+        singular: "-",
+        plural: "ēs",
+      },
+      genitive: {
+        singular: "is",
+        plural: "um",
+      },
+      dative: {
+        singular: "ī",
+        plural: "ibus",
+      },
+      accusative: {
+        singular: "em",
+        plural: "ēs",
+      },
+      ablative: {
+        singular: "e",
+        plural: "ibus",
+      },
+      vocative: {
+        singular: "-",
+        plural: "ēs",
+      },
     },
-    ablative: {
-      singular: "ā",
-      plural: "īs",
+    neuter_third_declension: {
+      nominative: {
+        singular: "-",
+        plural: "a",
+      },
+      genitive: {
+        singular: "is",
+        plural: "um",
+      },
+      dative: {
+        singular: "ī",
+        plural: "ibus",
+      },
+      accusative: {
+        singular: "-",
+        plural: "a",
+      },
+      ablative: {
+        singular: "e",
+        plural: "ibus",
+      },
+      vocative: {
+        singular: "-",
+        plural: "a",
+      },
     },
-    vocative: {
-      singular: "",
-      plural: "",
+    fourth_declension: {
+      nominative: {
+        singular: "us",
+        plural: "ūs",
+      },
+      genitive: {
+        singular: "ūs",
+        plural: "uum",
+      },
+      dative: {
+        singular: "uī",
+        plural: "ibus",
+      },
+      accusative: {
+        singular: "um",
+        plural: "ūs",
+      },
+      ablative: {
+        singular: "ū",
+        plural: "ibus",
+      },
+      vocative: {
+        singular: "us",
+        plural: "ūs",
+      },
+    },
+    neuter_fourth_declension: {
+      nominative: {
+        singular: "ū",
+        plural: "ua",
+      },
+      genitive: {
+        singular: "ūs",
+        plural: "uum",
+      },
+      dative: {
+        singular: "u",
+        plural: "ibus",
+      },
+      accusative: {
+        singular: "ū",
+        plural: "ua",
+      },
+      ablative: {
+        singular: "ū",
+        plural: "ibus",
+      },
+      vocative: {
+        singular: "ū",
+        plural: "ua",
+      },
+    },
+    fifth_declension: {
+      nominative: {
+        singular: "ēs",
+        plural: "ēs",
+      },
+      genitive: {
+        singular: "eī",
+        plural: "ērum",
+      },
+      dative: {
+        singular: "eī",
+        plural: "ēbum",
+      },
+      accusative: {
+        singular: "em",
+        plural: "ēs",
+      },
+      ablative: {
+        singular: "ē",
+        plural: "ēbus",
+      },
+      vocative: {
+        singular: "ēs",
+        plural: "ēs",
+      },
     },
   },
-  /*   second_declension: {
-    nominative: {
-      singular: "",
-      plural: "",
-    },
-    genitive: {
-      singular: "",
-      plural: "",
-    },
-    dative: {
-      singular: "",
-      plural: "",
-    },
-    accusative: {
-      singular: "",
-      plural: "",
-    },
-    ablative: {
-      singular: "",
-      plural: "",
-    },
-    vocative: {
-      singular: "",
-      plural: "",
-    },
-  }, */
 };
