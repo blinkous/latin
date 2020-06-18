c4fa17ab888a43770417f727371a21df0f05799f
blinkous <saradhani1@gmail.com>
2020-06-18 13:38:33 -0400

    added styles to declension tables

--- src/components/DeclensionTable.js
+++ src/components/DeclensionTable.js
@@ -3,18 +3,21 @@ import "../styles/DeclensionTable.css";
 import declensions from "../js/declensions";
 
 const DeclensionTable = (props) => {
-  const firstDecl = declensions.declensions.first_declension;
   const allDeclensions = declensions.declensions;
-
   return (
     <>
       {Object.entries(allDeclensions).map(([key, value], index) => (
         <table key={index} className={`declension-table ${key}`}>
-          <caption>
+          <caption className={`${key}-caption declension-table-title`}>
             {key
               .replace(/_/g, " ")
               .replace(/^[a-z]|\s\w/g, (match) => match.toUpperCase())}
           </caption>
+          <colgroup>
+            <col className="case-column" />
+            <col className="singular-column" />
+            <col className="plural-column" />
+          </colgroup>
           <thead>
             <tr className="header">
               <th className="case">Cases</th>
@@ -25,7 +28,7 @@ const DeclensionTable = (props) => {
           <tbody>
             {Object.entries(allDeclensions[key]).map(([key, value], index) => (
               <tr className={key} key={index}>
-                <td className={`case ${key}`}>{key}</td>
+                <th className={`case ${key}`}>{key}</th>
                 <td className={`singular ${value.singular}`}>
                   {value.singular}
                 </td>
