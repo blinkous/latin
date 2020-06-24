ae6ac723b97eb0fca8d06280e54e256ba42c6acc
blinkous <saradhani1@gmail.com>
2020-06-24 12:57:37 -0400

    modified accent colors, added description tooltips to the noun, pronoun, adjective and article in the declension definition

--- src/App.js
+++ src/App.js
@@ -12,8 +12,48 @@ const App = () => {
         <header>
           <h2 className="heading">Declensions</h2>
           <p>
-            The variation of a noun, pronoun, adjective or article to indicate
-            grammatical case, number and gender.
+            The variation of a{" "}
+            <span className="keyword">
+              noun
+              <span className="info">
+                A person, place or thing.
+                <span className="example">
+                  Aislin, Gemma, New York, cat, audience
+                </span>
+              </span>
+            </span>
+            ,{" "}
+            <span className="keyword">
+              pronoun
+              <span className="info">
+                A word that is used instead of a noun or noun phrase
+                <span className="example">
+                  I, me, he, she, herself, you, it, that, they, each, few, many,
+                  who, whoever, whose, someone, everybody
+                </span>
+              </span>
+            </span>
+            ,{" "}
+            <span className="keyword">
+              adjective
+              <span className="info">
+                A word or phrase that describes a noun
+                <span className="example">
+                  The <em>red</em> balloon. The <em>melodic</em> sounds. The
+                  grass is <em>green</em> here.
+                </span>
+              </span>
+            </span>{" "}
+            or{" "}
+            <span className="keyword">
+              article
+              <span className="info">
+                Special modifiers that appear before nouns or noun phrases that
+                help clarify the meaning of them.
+                <span className="example">the, a/an</span>
+              </span>
+            </span>{" "}
+            to indicate grammatical case, number and gender.
           </p>
         </header>
         {Object.entries(allDeclensions).map(([key, value], index) => (
