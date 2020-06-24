6ccf2f8e22aea98df7f4ebbe289f5aadbde92d51
blinkous <saradhani1@gmail.com>
2020-06-24 13:10:06 -0400

    Updates

---
+++ precache-manifest.e453c643489d1fe67e0b78576eaaec18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd95b6473a5a8b20b15ec9a297e08ec9",
+    "url": "/latin/index.html"
+  },
+  {
+    "revision": "11b56ce7f9ba3448a385",
+    "url": "/latin/static/css/main.0710f2b8.chunk.css"
+  },
+  {
+    "revision": "dfb47aeecea335574e15",
+    "url": "/latin/static/js/2.e450832b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/latin/static/js/2.e450832b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b56ce7f9ba3448a385",
+    "url": "/latin/static/js/main.12eff5dd.chunk.js"
+  },
+  {
+    "revision": "831afd0af45c13cdcdcc",
+    "url": "/latin/static/js/runtime-main.a96291be.js"
+  }
+]);
