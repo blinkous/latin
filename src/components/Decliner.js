a3142e45938159979f65b8d0780eb4f10848ef54
blinkous <saradhani1@gmail.com>
2020-07-14 18:41:50 -0400

    added more styling for the decliner and made everything border-box by default

--- src/components/Decliner.js
+++ src/components/Decliner.js
@@ -5,7 +5,9 @@ import "../styles/Decliner.css";
 
 export const Decliner = () => {
   const [showDeclined, setShowDeclined] = useState(false);
-  const [root, setRoot] = useState("");
+  const [stem, setStem] = useState("");
+  const [nominativeForm, setNominativeForm] = useState("");
+  const [gentiveForm, setGentiveForm] = useState("");
   const [currDeclension, setCurrDeclension] = useState("first_declension");
 
   const allDeclensions = declensions.declensions;
@@ -21,14 +23,14 @@ export const Decliner = () => {
     "Fifth",
   ];
 
-  const handleRootChange = (e) => {
+  const handleStemChange = (e) => {
     const val = e.target.value;
-    console.log(val);
     if (val !== "") {
       setShowDeclined(true);
-      setRoot(val);
+      setStem(val);
     }
   };
+
   const handleDecChange = (e) => {
     const decl = `${e.target.value}_declension`;
     setCurrDeclension(decl);
@@ -44,19 +46,65 @@ export const Decliner = () => {
     ></DeclensionTable>
   );
 
+  const handleNomChange = (e) => {
+    const val = e.target.value;
+    if (stem === "") {
+      if (val !== "") {
+        setNominativeForm(val);
+        if (gentiveForm !== "") {
+          setShowDeclined(true);
+        }
+      }
+    }
+  };
+
+  const handleGenChange = (e) => {
+    const val = e.target.value;
+    if (stem === "") {
+      if (val !== "") {
+        setGentiveForm(val);
+        if (nominativeForm !== "") {
+          setShowDeclined(true);
+        }
+      }
+    }
+  };
+
+  const findStem = (nomForm, genForm) => {
+    /* Future add support for both special and non-special char */
+  };
+
   return (
     <div className="decliner">
       <form className="decliner-form">
         <input
           type="text"
-          name="root_word"
-          className="decliner-root-word"
-          onChange={handleRootChange}
+          name="nominative_word"
+          className="decliner-nominative-word decliner-field"
+          onChange={handleNomChange}
+          placeholder="Nominative Case"
+          title="Nominative Case"
+        />
+        <input
+          type="text"
+          name="genitive_word"
+          className="decliner-genitive-word decliner-field"
+          onChange={handleGenChange}
+          placeholder="Genitive Case"
+          title="Genitive Case"
+        />
+        <p>or</p>
+        <input
+          type="text"
+          name="stem_word"
+          className="decliner-stem-word decliner-field"
+          onChange={handleStemChange}
           placeholder="Latin Stem"
+          title="Latin Stem"
         />
         <select
           name="declension_select"
-          className="decliner-declension-select"
+          className="decliner-declension-select decliner-field"
           onChange={handleDecChange}
         >
           {declensionOptions.map((el, index) => (
@@ -66,7 +114,7 @@ export const Decliner = () => {
           ))}
         </select>
       </form>
-      {showDeclined && buildDeclinedTables(root, currDeclension)}
+      {showDeclined && buildDeclinedTables(stem, currDeclension)}
     </div>
   );
 };
