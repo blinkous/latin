f477f0c82ae4caae7ee97533d0c3ebbe007ce56d
blinkous <saradhani1@gmail.com>
2021-05-25 21:49:59 -0400

    Updates

---
+++ precache-manifest.bb0d59a42d355a7315de3d91fc0c1bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8e6fc82f2a9733434c1027d3e03129",
+    "url": "/latin/index.html"
+  },
+  {
+    "revision": "4fc60860de745de35602",
+    "url": "/latin/static/css/main.12359a73.chunk.css"
+  },
+  {
+    "revision": "6115b0a375a0810a8a9f",
+    "url": "/latin/static/js/2.98feed03.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/latin/static/js/2.98feed03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc60860de745de35602",
+    "url": "/latin/static/js/main.aa7e87b4.chunk.js"
+  },
+  {
+    "revision": "b517487d81134f7ba692",
+    "url": "/latin/static/js/runtime-main.d46f98c5.js"
+  }
+]);
